59cb2718a8ce24326e84dc0719ed0f1a50bb159b
mtop <mats@topel.se>
2012-06-18 15:27:17 +0000

    Removed dev. comments from code

--- lib/Filters.py
+++ lib/Filters.py
@@ -18,24 +18,19 @@ import UserInput
 
 uid = UserInput.Data()
 
-""" Only include species that have been recorded 
+""" 
+	Only include species that have been recorded 
 	in the polygon >= times what is indicated in 
-	'occurrence_nr' """
+	'occurrence_nr' 
+"""
 def occurrence_nr(raw_species_list):
 	# Test if this filter should be used
-#	print uid.occurrence_nr						# Devel.
 	if uid.occurrence_nr != 0:
-		print "[--] occurrence_nr is not zero"		# Devel.
 		species_list = []
 		for record in set(raw_species_list):
-#			print record							# Devel.
-#			print raw_species_list.count(record)	# Devel.
 			if int(raw_species_list.count(record)) >= int(uid.occurrence_nr) and record not in species_list:
 				species_list.append(record)
-#				print "%s occurres more than %s times" % (record, uid.occurrence_nr) # Devel
-#		print species_list
 		return species_list
 	else:
-		print "[WW] occurrence_nr is zero"
 		return raw_species_list
 
--- neotropics_pipeline.py
+++ neotropics_pipeline.py
@@ -19,23 +19,9 @@ for polygon in uid.user_polygons.split(':'):
 	#print c_all[0]										# Devel.
 	#print c_some										# Devel.
 
-	#c_all = ['IS']										# Devel.
 	""" Create a list of all species found in a 
 		user defined polygon """
 	raw_species_list = QueryGBIF.qgd(c_all[0])			# Working
-#	print str(raw_species_list)							# Devel.
-#	print raw_species_list.count(2038894)				# Devel. 
-	print "raw_species_list contains %s records" % len(raw_species_list)
-#	Filters.occurrence_nr(raw_species_list)				# Working	
+	print "raw_species_list contains %s records" % len(raw_species_list)	# Devel.
 	print "species_list contains %s records" % len(Filters.occurrence_nr(raw_species_list))		# Devel.
-
-
-	#code = 'IS'										# Devel.
-	#print QueryGBIF.qgd(code)								# Devel.
-	#print QueryGBIF.raw_species_list(code)			# Devel.
-
-	#print c[0]	# Devel
-	#print c[1]	# Devel
-	#print Countries.some_species(c[1])	# Devel
-
 
