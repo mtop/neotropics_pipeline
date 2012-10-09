29e6f0721872e357eba1f1c353527624474fb80d
mtop <mats@topel.se>
2012-10-09 12:01:40 +0000

    cleaned up the 'neotropics_pipeline.py' script

--- neotropics_pipeline.py
+++ neotropics_pipeline.py
@@ -8,42 +8,37 @@ import SpeciesInPolygon as sip
 
 uid = UserInput.Data()
 
-"""     For each user defined polygon, Identify 
-		countries from where all species should 
-		be included in downstream analyses,
-		and countries where only some species 
-		should be included.
-"""
 def generate_country_lists():
+""" For each user defined polygon, Identify 
+	countries from where all species should 
+	be included in downstream analyses,
+	and countries where only some species 
+	should be included."""
 	for polygon in uid.user_polygons.split(':'):
-		c_all = Countries.all_species(polygon)				# Working # Include all species from these countries 
-		c_some = Countries.some_species(c_all[1])			# Working # Incluse some species from these countries
+		c_all = Countries.all_species(polygon)				# Working # Includes all species from these countries 
+		c_some = Countries.some_species(c_all[1])			# Working # Includes some species from these countries
 		print c_all[0]										# Devel.
 		return c_all, c_some
 		#print c_some										# Devel.
 
 
-""" Create a list of all species found in a 
-	user defined polygon 
-"""
 def generate_species_list(c_all, c_some):
+""" Create a list of all species found in a 
+	user defined polygon.""" 
 	raw_species_list_plants, raw_species_list_animals, raw_species_list_fungi = QueryGBIF.qgd(c_all)
 #	raw_species_list = QueryGBIF.qgd(c_all)
 #	raw_species_list = QueryGBIF.qgd(c_all[0])				# Working
 	species_list_plants = Filters.occurrence_nr(raw_species_list_plants)
+	print species_list_plants		# Devel.
 	species_list_animals = Filters.occurrence_nr(raw_species_list_animals)
 	species_list_fungi = Filters.occurrence_nr(raw_species_list_fungi)
 #	species_list = Filters.occurrence_nr(raw_species_list)
 	return species_list_plants, species_list_animals, species_list_fungi
 
-""" Create an output file to store the 
-"species_list" in 
-"""
 
-""" Convert the numerical species list 
-to a list containing species names 
-"""
 def numbers_to_names(species_list, sufix):
+""" Convert the numerical species list 
+	to a list containing species names.""" 
 	filtered_species_list =[]
 	for number in species_list:
 		name = QueryGBIF.species_name(number)
@@ -55,9 +50,8 @@ def numbers_to_names(species_list, sufix):
 
 
 def write_output_file(filtered_species_list, sufix):
+"""	Write species list to file."""		
 	for name in filtered_species_list:
-		
-
 #		print str(species_name[0]), str(species_name[1])
 #		name = str(species_name[0]), str(species_name[1])	# Remove ????
 #		print name								# Devel.
